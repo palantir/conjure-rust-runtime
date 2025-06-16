622fc97d3d08b844465546671882a3612c48421a
Steven Fackler <sfackler@palantir.com>
2025-06-15 21:35:59 -0400

    Replace unsafe intrusive list with pin-list

--- conjure-runtime/src/service/node/limiter/deficit_semaphore.rs
+++ conjure-runtime/src/service/node/limiter/deficit_semaphore.rs
@@ -12,11 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 use parking_lot::Mutex;
+use pin_list::{Node, NodeData, PinList};
 use pin_project::{pin_project, pinned_drop};
 use std::future::Future;
-use std::marker::PhantomPinned;
 use std::pin::Pin;
-use std::ptr::NonNull;
 use std::sync::Arc;
 use std::task::{Context, Poll, Waker};
 
@@ -30,6 +29,10 @@ use std::task::{Context, Poll, Waker};
 ///
 /// It's implemented with an intrusive linked list of waiters, taking advantage of the fact that futures are pinned in
 /// memory.
+///
+/// To ensure that the semaphore is absolutely fair, the only thing that can remove a waiter's node from the queue is
+/// the waiter itself. Otherwise, a newer waiter could see the queue as empty and take a permit before an older waiter
+/// that was woken had a chance to take it.
 pub struct DeficitSemaphore(Mutex<State>);
 
 impl DeficitSemaphore {
@@ -37,8 +40,7 @@ impl DeficitSemaphore {
         Arc::new(DeficitSemaphore(Mutex::new(State {
             total_permits: permits,
             outstanding_permits: 0,
-            head: None,
-            tail: None,
+            waiters: PinList::new(pin_list::id::Checked::new()),
         })))
     }
 
@@ -62,17 +64,22 @@ impl DeficitSemaphore {
     pub fn acquire(self: Arc<Self>) -> Acquire {
         Acquire {
             semaphore: self,
-            node: None,
-            _p: PhantomPinned,
+            node: Node::new(),
         }
     }
 }
 
+type PinListTypes = dyn pin_list::Types<
+    Id = pin_list::id::Checked,
+    Protected = Option<Waker>,
+    Removed = (),
+    Unprotected = (),
+>;
+
 struct State {
     total_permits: usize,
     outstanding_permits: usize,
-    head: Option<NonNull<Node>>,
-    tail: Option<NonNull<Node>>,
+    waiters: PinList<PinListTypes>,
 }
 
 impl State {
@@ -82,100 +89,39 @@ impl State {
 
     fn maybe_wake(&mut self) {
         if self.has_permits() {
-            if let Some(head) = &mut self.head {
-                unsafe {
-                    if let Some(waker) = head.as_mut().waker.take() {
-                        waker.wake();
-                    }
-                }
+            if let Some(waker) = self
+                .waiters
+                .cursor_front_mut()
+                .protected_mut()
+                .and_then(|o| o.take())
+            {
+                waker.wake();
             }
         }
     }
 }
-
-struct Node {
-    waker: Option<Waker>,
-    next: Option<NonNull<Node>>,
-    prev: Option<NonNull<Node>>,
-}
-
-impl Node {
-    // SAFETY: this node must be in `state`'s queue.
-    unsafe fn dequeue(&mut self, state: &mut State) {
-        let self_ptr = NonNull::from(&mut *self);
-
-        match &mut self.prev {
-            Some(prev) => {
-                debug_assert_eq!(prev.as_mut().next, Some(self_ptr));
-                prev.as_mut().next = self.next;
-            }
-            None => {
-                debug_assert_eq!(state.head, Some(self_ptr));
-                state.head = self.next;
-            }
-        }
-
-        match &mut self.next {
-            Some(next) => {
-                debug_assert_eq!(next.as_mut().prev, Some(self_ptr));
-                next.as_mut().prev = self.prev;
-            }
-            None => {
-                debug_assert_eq!(state.tail, Some(self_ptr));
-                state.tail = self.prev;
-            }
-        }
-
-        self.prev = None;
-        self.next = None;
-    }
-
-    // SAFETY: This node must not currently be in a queue and must remain alive at the same address until it is removed
-    // from the queue.
-    unsafe fn push_back(&mut self, state: &mut State) {
-        debug_assert_eq!(self.next, None);
-        debug_assert_eq!(self.prev, None);
-
-        match &mut state.tail {
-            Some(tail) => {
-                debug_assert_eq!(tail.as_mut().next, None);
-                tail.as_mut().next = Some(self.into());
-                self.prev = state.tail;
-            }
-            None => state.head = Some(self.into()),
-        }
-
-        state.tail = Some(self.into());
-    }
-}
-
-unsafe impl Sync for State {}
-unsafe impl Send for State {}
 
 #[pin_project(PinnedDrop)]
 pub struct Acquire {
     semaphore: Arc<DeficitSemaphore>,
-    node: Option<Node>,
     #[pin]
-    _p: PhantomPinned,
+    node: Node<PinListTypes>,
 }
-
-unsafe impl Sync for Acquire {}
-unsafe impl Send for Acquire {}
 
 #[pinned_drop]
 impl PinnedDrop for Acquire {
     fn drop(self: Pin<&mut Self>) {
         let this = self.project();
 
-        if let Some(node) = this.node {
-            let mut state = this.semaphore.0.lock();
-            let is_head = node.prev.is_none();
-            unsafe {
-                node.dequeue(&mut state);
-            }
-            // if we were up next but dropped before taking our permit, wake the next waiter
-            if is_head {
+        let Some(node) = this.node.initialized_mut() else {
+            return;
+        };
+
+        let mut state = this.semaphore.0.lock();
+
+        // if we were up next but dropped before taking our permit, wake the next waiter
+        if let (NodeData::Linked(waker), ()) = node.reset(&mut state.waiters) {
+            if waker.is_none() {
                 state.maybe_wake();
             }
         }
@@ -186,59 +132,53 @@ impl Future for Acquire {
     type Output = Permit;
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let this = self.project();
+        let mut this = self.project();
 
         let mut state = this.semaphore.0.lock();
 
         // fast path where no one's queued
-        if state.head.is_none() && state.has_permits() {
+        if state.waiters.is_empty() && state.has_permits() {
             state.outstanding_permits += 1;
             return Poll::Ready(Permit {
                 semaphore: this.semaphore.clone(),
             });
         }
 
-        let node = match this.node {
-            Some(node) => node,
-            None => {
-                *this.node = Some(Node {
-                    waker: None, // will be filled in below
-                    next: None,
-                    prev: None,
-                });
+        match this.node.as_mut().initialized_mut() {
+            Some(node) => {
+                match node.protected_mut(&mut state.waiters) {
+                    // we're still waiting
+                    Some(Some(waker)) => {
+                        // if the future jumped executors (uncommon but possible), update the waker
+                        if !waker.will_wake(cx.waker()) {
+                            *waker = cx.waker().clone();
+                        }
+                        Poll::Pending
+                    }
+                    // we've been woken, so it's our turn to take a permit
+                    Some(None) => {
+                        node.unlink(&mut state.waiters)
+                            .expect("node is in the list");
+                        state.outstanding_permits += 1;
+                        // construct this up front just in case maybe_wait panics to avoid leaking the permit
+                        let permit = Permit {
+                            semaphore: this.semaphore.clone(),
+                        };
 
-                let node = this.node.as_mut().unwrap();
-                unsafe {
-                    node.push_back(&mut state);
+                        state.maybe_wake();
+
+                        Poll::Ready(permit)
+                    }
+                    // We haven't queued yet
+                    None => panic!("future polled after completion"),
                 }
-
-                node
             }
-        };
-
-        // only the waiter at the head of the queue can take a permit
-        if node.prev.is_none() && state.has_permits() {
-            unsafe {
-                node.dequeue(&mut state);
+            None => {
+                state
+                    .waiters
+                    .push_back(this.node, Some(cx.waker().clone()), ());
+                Poll::Pending
             }
-            *this.node = None;
-
-            state.outstanding_permits += 1;
-            // construct this up front just in case maybe_wait panics to avoid leaking the permit
-            let permit = Permit {
-                semaphore: this.semaphore.clone(),
-            };
-
-            state.maybe_wake();
-
-            Poll::Ready(permit)
-        } else {
-            // if the future jumped executors (uncommon but possible), update the waker
-            if !node.waker.as_ref().is_some_and(|w| w.will_wake(cx.waker())) {
-                node.waker = Some(cx.waker().clone());
-            }
-
-            Poll::Pending
         }
     }
 }
