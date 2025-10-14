79e57b053560ad90fbabefd104c38c7dfceeab28
Steven Fackler <sfackler@palantir.com>
2025-10-14 13:47:30 -0400

    fmt

--- conjure-runtime/src/blocking/body.rs
+++ conjure-runtime/src/blocking/body.rs
@@ -129,8 +129,7 @@ impl BodyWriter {
     }
 
     fn send(&mut self, message: BodyPart) -> io::Result<()> {
-        executor::block_on(self.sender.send(message))
-            .map_err(io::Error::other)
+        executor::block_on(self.sender.send(message)).map_err(io::Error::other)
     }
 
     /// Writes a block of body bytes.
